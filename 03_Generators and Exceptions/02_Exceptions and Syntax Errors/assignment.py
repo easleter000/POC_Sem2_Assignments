9e2bfec00b65928a6b1faef747d7905b3df78b53
Terry Easley <easleter000@warren.k12.in.us>
2024-01-23 09:47:14 -0500

    U3L2

--- 03_Generators and Exceptions/02_Exceptions and Syntax Errors/assignment.py
+++ 03_Generators and Exceptions/02_Exceptions and Syntax Errors/assignment.py
@@ -2,9 +2,8 @@ try:
     value = int(input('Enter an integer: '))
     value_1 = int(input('Enter an 2nd integer: '))
     print('The quotient of', value ,"and", value_1, 'is', value/value_1)
-except ValueError:
-    print('You did not provide a number, so I will not calculate the quotient')
 except ZeroDivisionError:
     print('You provided 0 and division by 0 is not possible, sorry')
-
+except:
+    print("Something strange happened")
     
