13fde729ff2d4d6ff16276c8be81e5efee62c4a6
Terry Easley <easleter000@warren.k12.in.us>
2024-02-06 09:27:12 -0500

    U6L3

---
+++ 06_Types, Modules, and Packages/02_Modules, Import and Math/assignment.py
@@ -0,0 +1,9 @@
+
+leg1 = float(input("Enter the value of the 1st leg:"))
+leg2 =  float(input("Enter the value of the 2nd leg:"))
+
+import math
+
+hyp = math.hypot(leg1, leg2)
+
+print("The hypotenuse is", hyp)
