6770df04aaefd14d8c9294b763020e1a675d1c2d
Terry Easley <easleter000@warren.k12.in.us>
2024-02-29 09:46:34 -0500

    M8L1

--- 08_OOP/01_Intro/assignment.py
+++ 08_OOP/01_Intro/assignment.py
@@ -1,14 +1,22 @@
+import math
+
+
 class RightTriangle: 
     def __init__(self, base, height):
         self.base = base
-        #You do the same thing for the height
-        pass #You do:  remove this pass when done.  
-    
+        self.height = height
+        
+
     def area(self):
-        #You do:  return the area calculated from the formula 1/2 base * height.  
-        #You do:  remember to access the base you need to use self.base, same for the height
-        pass  # You do:  remove this pass when done.
+        return (1/2 * self.base * self.height)
+    
+    
+    
 
 triangle_1 = RightTriangle(3, 4)
 print("The area of triange_1 is", triangle_1.area())
-#Do the same thing for another triangle.  
+ 
+tri2 = RightTriangle(5, 5)
+print("The base of tri2 is", tri2.base)
+print("The height of tri2 is", tri2.height)
+print("The area of tri2 is", tri2.area())
